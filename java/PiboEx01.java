edf88ccf5a2c0d30077dc26671295a376513603e
js <solwish90@gmail.com>
2017-09-03 08:53:22 +0900

    Fibonacci numbers

---
+++ java/PiboEx01.java
@@ -0,0 +1,26 @@
+package zedu05;
+
+public class PiboEx01 {
+
+	public static void main(String[] args) {
+		
+//		 1 + 1 + 2 + 5 + 8 + 13 + ... 의 순서로 나열되는
+//		피보나치수열의 10번째 항까지의 합계를 구하는 순서도를 작성하시오.
+		
+		int a = 1;
+		int b = 1;
+		int sum = 2;
+				
+		for(int i=2;i<=9; i++){
+			int c = a+b;
+			sum = sum + c;
+			a = b;
+			b = c;
+		}
+		
+		System.out.println("10번째 항까지의 합은 " + sum);
+		
+		
+	}
+
+}
