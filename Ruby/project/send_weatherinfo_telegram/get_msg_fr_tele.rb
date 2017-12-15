ca289877b06709938ecdc620f9f77ccf37154947
js <solwish90@gmail.com>
2017-12-15 21:11:33 +0900

    fix infinity sending messages

--- Ruby/project/send_weatherinfo_telegram/get_msg_fr_tele.rb
+++ Ruby/project/send_weatherinfo_telegram/get_msg_fr_tele.rb
@@ -16,24 +16,31 @@ def sendMessage(message)
   HTTParty.get("#{$url}#{$token}/sendMessage?chat_id=#{$check_id}&text=#{message}")
 end
 
-# 1초 간격으로 보내는 코드
-while true
-  old_up = hash["result"].last["update_id"]
+def getOldUpdate()
   response = HTTParty.get("#{$url}#{$token}/getUpdates")
   hash = JSON.parse(response.body)
-  check_up = hash["result"].last["update_id"]
+  hash["result"].last["update_id"]
+end
+
+old_up = hash["result"].last["update_id"]
+# 1초 간격으로 보내는 코드
+while true
+
+  check_up = getOldUpdate()
 
     if check_up > old_up
-      old_up = check_up
       city = hash["result"].last["message"]["text"]
       city.capitalize!
       res = HTTParty.get("http://samples.openweathermap.org/data/2.5/weather?q=#{city},uk&appid=b6907d289e10d714a6e88b30761fae22")
       hash_wea = JSON.parse(res.body)
       main = hash_wea["weather"].last["main"]
       des = hash_wea["weather"].last["description"]
+      sendMessage(main)
+      sendMessage(des)
+
+      old_up = getOldUpdate
     end
 
-    sendMessage("#{main} #{des}")
     Post.first_or_create(:update_id => check_up)
   sleep(1)  #단위 초야
 end
