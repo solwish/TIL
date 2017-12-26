0428d6a0617db4307e423ca7a8a58fca8c26ac6a
solwish <solwish90@gmail.com>
2017-12-26 15:09:30 +0900

    making code like scaffold

---
+++ Ruby/project/scaffold_practice/app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # before_action(:set_post, {only: [:show, :edit, :update, :destroy]})
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params
+      # title: params["post"]["title"],
+      # content: params["post"]["content"]
+    )
+    if @post.save
+      redirect_to '/'
+    else
+      flash[:alert] = "제목과 내용은 필수입니다."
+      redirect_to :back
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to @post
+    # redirect_to "/posts/#{@post.id}"
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to '/'
+  end
+
+  #스코프 이 클래스 내부에서만 써줄거라. 위에 애들은 뷰랑 라우트에서 써줌
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
+end
---
+++ Ruby/project/scaffold_practice/app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, presence: true
+end
