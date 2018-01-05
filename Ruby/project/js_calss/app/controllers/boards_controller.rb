9e8571607846db964226c8b0db7b81624abe33b0
solwish <solwish90@gmail.com>
2018-01-05 10:32:05 +0900

    fin js_class

--- Ruby/project/js_calss/app/controllers/boards_controller.rb
+++ Ruby/project/js_calss/app/controllers/boards_controller.rb
@@ -1,6 +1,6 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :page_scroll]
 
   # GET /boards
   # GET /boards.json
