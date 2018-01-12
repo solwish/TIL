b409e95ce0eb9055c35066facdac5fc462078344
solwish <solwish90@gmail.com>
2018-01-12 15:45:23 +0900

    I18n, active admin, recaptcha

---
+++ Ruby/project/locale_practice/app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] #to_sym 하면 안되길래 뺐어
+  end
+
+end
---
+++ Ruby/project/locale_practice/config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.site_key  = 'my site key'
+  config.secret_key = 'my secret key'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
