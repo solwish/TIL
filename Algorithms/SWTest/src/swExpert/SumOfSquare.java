3242d5875ae31a0c65db24a8dabaed1fd0f363ee
js <solwish90@gmail.com>
2018-05-06 10:05:27 +0900

    til

--- Algorithms/SWTest/src/swExpert/SumOfSquare.java
+++ Algorithms/SWTest/src/swExpert/SumOfSquare.java
@@ -2,12 +2,13 @@ package swExpert;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
+import java.text.DecimalFormat;
 import java.util.StringTokenizer;
 
 public class SumOfSquare {
 
 	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 
@@ -25,11 +26,22 @@ public class SumOfSquare {
 				A[i][0] = Integer.parseInt(sss);
 				A[i][1] = ss.charAt(ss.length() - 1) - 48;
 			}
-			BigInteger sum = BigInteger.ZERO;
-			for (int i = 0; i < n; i++)
-				sum = sum.add(BigInteger.valueOf((long) Math.pow(A[i][0], A[i][1])));
 
-			System.out.println("#" + t_case + " " + sum);
+			double sum = 0;
+			double tmp = 0;
+			for (int i = 0; i < n; i++)
+				sum += Math.pow(A[i][0], A[i][1]);
+			// {
+			// tmp = 1;
+			// for (int j = 0; j < A[i][1]; j++) {
+			// tmp *= A[i][0];
+			// }
+			// sum += tmp;
+			// }
+			DecimalFormat decimalFormat = new DecimalFormat("#.##");
+			String ans = decimalFormat.format(sum);
+
+			System.out.println("#" + t_case + " " + ans);
 		}
 	}
 
