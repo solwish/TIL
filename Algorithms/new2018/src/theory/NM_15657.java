0f7a8e9a3f394c47ac9b741c193665310fd965a4
js <solwish90@gmail.com>
2018-09-28 00:23:34 +0900

    review NM

---
+++ Algorithms/new2018/src/theory/NM_15657.java
@@ -0,0 +1,43 @@
+package theory;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class NM_15657 {
+	static Vector<Integer> V = new Vector<>();
+	static int table[], N, M;
+	static StringBuffer sb = new StringBuffer();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		table = new int[N + 1];
+		for (int i = 1; i <= N; i++)
+			table[i] = sc.nextInt();
+		Arrays.sort(table);
+
+		dfs(1);
+
+		System.out.println(sb);
+	}
+
+	private static void dfs(int start) {
+		if (V.size() == M) {
+			for (int v : V)
+				sb.append(v + " ");
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = start; i <= N; i++) {
+			V.add(table[i]);
+			dfs(i);
+			V.remove(V.size() - 1);
+		}
+
+	}
+
+}
---
+++ Algorithms/new2018/src/theory/NM_15664.java
@@ -0,0 +1,56 @@
+package theory;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class NM_15664 {
+	static Vector<Integer> V = new Vector<>();
+	static int table[], N, M;
+	static StringBuffer sb = new StringBuffer();
+	static boolean visit[];
+	static HashSet<String> set = new HashSet<>();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		visit = new boolean[N + 1];
+		table = new int[N + 1];
+		for (int i = 1; i <= N; i++)
+			table[i] = sc.nextInt();
+		Arrays.sort(table);
+
+		dfs(1);
+
+		System.out.println(sb);
+	}
+
+	private static void dfs(int start) {
+		if (V.size() == M) {
+			String s = "";
+			for (int v : V)
+				s += String.valueOf(v + " ");
+			if (!set.contains(s)) {
+				sb.append(s);
+				sb.append("\n");
+				set.add(s);
+			}
+			return;
+		}
+
+		for (int i = start; i <= N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				V.add(table[i]);
+				dfs(i + 1);
+				V.remove(V.size() - 1);
+				visit[i] = false;
+			}
+		}
+
+	}
+
+}
